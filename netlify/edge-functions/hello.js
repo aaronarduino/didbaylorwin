a9ea16fd9d4ec7927529ecb5e4af7473333e566e
Aaron Alexander <aarona.arduino@gmail.com>
2022-11-18 00:09:53 -0600

    init commit

---
+++ netlify/edge-functions/hello.js
@@ -0,0 +1,16 @@
+export default async (request, context) => {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+  return await fetch('https://data.ncaa.com/casablanca/scoreboard/basketball-men/d1/' + yyyy + '/' + mm + '/' + dd + '/scoreboard.json')
+    .then((response) => response.json())
+    .then((data) => {
+      let game = data.games.find((g) => g.game.title.includes("Baylor"));
+      if (!game) {
+        return context.json({validFor: today, team: {}});
+      }
+      let baylor = game.game.home.names.full.includes("Baylor") ? game.game.home : game.game.away;
+      return context.json({validFor: today, team: baylor});
+    });
+};
