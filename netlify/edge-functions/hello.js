ae03e47ce22b812ae6765e07df0ada268e73b8ff
Aaron Alexander <aarona.arduino@gmail.com>
2022-11-23 00:10:56 -0600

    fix: actually return just date when baylor not on sched

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -6,7 +6,7 @@ export default async (request, context) => {
     .then((data) => {
       let game = data.games.find((g) => g.game.title.includes("Baylor"));
       if (!game) {
-        return context.json({validFor: today, team: {}});
+        return context.json({validFor: today.today, team: {}});
       }
       let baylor = game.game.home.names.full.includes("Baylor") ? game.game.home : game.game.away;
       console.log({validFor: today.today, team: baylor});
